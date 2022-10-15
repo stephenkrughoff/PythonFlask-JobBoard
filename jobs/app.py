326c59f0776eba443b057487fb9550313bb9c85e
Stephen Krughoff <s.krughoff@insidepetroleum.com>
2022-10-14 18:12:53 -0700

    add job link

--- jobs/app.py
+++ jobs/app.py
@@ -6,12 +6,14 @@ PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+
 def open_connection():
     connection: Optional[sqlite3.Connection] = getattr(g, '_connection', None)
     if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -23,6 +25,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection: Optional[sqlite3.Connection] = getattr(g, '_connection', None)
@@ -33,6 +36,18 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
-                       'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    jobs = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
+        'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id'
+    )
     return render_template('index.html', jobs=jobs)
+
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
+        'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id '
+        'WHERE job.id = ?', [job_id],
+        single=True)
+    return render_template('job.html', job=job)
